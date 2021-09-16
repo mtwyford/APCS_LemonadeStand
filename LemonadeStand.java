43b48bc4bac4c98203ed062493d8f714778e3e9c
Marcus T <mtwyford@users.noreply.github.com>
2021-09-16 09:54:26 -0400

    Update LemonadeStand.java

--- LemonadeStand.java
+++ LemonadeStand.java
@@ -1,7 +1,67 @@
-// Main class file for your program
+		//Starter Code from Class
 
+		/*Methods?
+		makeLemonade?
+		sellLemonade?
+		shrink? lose an item  - ice melts, lemons rot, 
+		dailyBatch?
+		*/
 public class LemonadeStand {
-  
-  
-  
+
+		int numLemons;
+		int cupsSugar;
+		int cupIce;
+		int numCups;		//recipe??
+		int numPitchers;
+		int numCustomers;  //
+		double pricePerCup;
+		double weatherScore; //think about
+
+		//CONSTRUCTOR
+		public LemonadeStand() {
+			numLemons = 9;
+			cupsSugar = 9;
+			cupsIce=9;
+			
+		}
+
+
+
+		public void makeLemonade() {  //DEFAULT METHOD
+			//recipe? 3 numLemons + 3 cupsSugar + 3 cupsIce
+			//to make a pitcher, add 1 to the count of num pitcher
+			if (numLemons>=3 && cupsSugar>=3 && cupsIce >=3) {
+				numPitchers++;
+				numLemons-=3;  //numLemons = numLemons - 3
+				cupsSugar-=3;
+				cupsIce-=3;
+			}
+		}
+		/* OVERLOADED METHOD - a 'custom method' passing in arguments ** */										
+		public void makeLemonade(int nL, int cS, int cI) {
+			//recipe? 3 numLemons + 3 cupsSugar + 3 cupsIce
+			//to make a pitcher, add 1 to the count of num pitcher
+			if (numLemons>=nL && cupsSugar>=cS && cupsIce >=cI) {
+				numPitchers++;
+				numLemons-=nL;  //numLemons = numLemons - 3
+				cupsSugar-=cS;
+				cupsIce-=cI;
+			}
+		}
+		
+		public void sellLemonade() {
+			//  ? how many pitchers did I make?
+			// how many cups in a pitcher
+			//how many cups total available to pour??
+			
+			cupsMade--;
+			//print out num cups && pitchers left??
+		}
+		
+	
+		
+		
+		
+		
+		
 }
